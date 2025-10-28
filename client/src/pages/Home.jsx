21c13820effb25b95a47656f91cccde3a9f255a0
vikasvkori1290 <vikasvkori129@gmail.com>
2025-10-28 23:15:31 +0530

    Feature section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/home/Banner'
 import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
 
 const Home = () => {
   return (
     <div>
         <Banner/>
         <Hero/>
+        <Features/>
     </div>
   )
 }
