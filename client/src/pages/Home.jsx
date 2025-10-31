03552d00ffbd0df7005fb5b6c4ef5585648ba426
vikasvkori1290 <vikasvkori129@gmail.com>
2025-10-31 22:53:16 +0530

    Footer of home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Banner from '../components/home/Banner'
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
 import Testimonial from '../components/home/Testimonial';
+import CallToAction from '../components/home/CallToAction';
+import Footer from '../components/home/Footer';
 
 
 const Home = () => {
@@ -12,6 +14,8 @@ const Home = () => {
         <Hero/>
         <Features/>
         <Testimonial/>
+        <CallToAction/>
+        <Footer/>
     </div>
   )
 }
